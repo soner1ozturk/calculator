3f678f29dd32abd7f46d0220d4c9898419d413a0
soner <soner.ozturk@torontomu.ca>
2025-09-23 14:54:18 -0400

    created first div calculator container and made reference to js and css files in html

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let calculatorContainer = createElement('div');
+calculatorContainer.className = 'calculatorContainer';
+let body = document.querySelector('body');
+body.appendChild(calculatorContainer);
